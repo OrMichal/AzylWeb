8e3013d8deda19cd537c74e3ff713858d1815e7d
OrMichal <misaorsah5@gmail.com>
2025-02-08 14:37:15 +0100

    přidal jsem zápatí, menší popisky a upravil jsem trochu navBar. Ještě budu muset trochu pořešit velikost navbaru po opuštění jeho původní pozice.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,3 +2,12 @@ window.onload = () => {
     const cartValue = document.getElementById("cartValue");
     cartValue.textContent = "0kč";
 };
+
+window.addEventListener("scroll", () => {
+    const nav = document.querySelector(".main-nav");
+    if(window.scrollY > 200){
+        nav.id = "fixed-nav";
+    } else{
+        nav.id = "";
+    }
+});

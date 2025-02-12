5bae0f348c03328cda7b380135b41da8bca04701
OrMichal <misaorsah5@gmail.com>
2025-02-12 19:44:45 +0100

    přidal jsem kontaktní formulář

--- public/scripts/navbar-switcher.js
+++ public/scripts/navbar-switcher.js
@@ -5,9 +5,6 @@ window.onload = () => {
 
 window.addEventListener("scroll", () => {
     const nav = document.querySelector(".main-nav");
-    if(window.scrollY > 200){
-        nav.id = "fixed-nav";
-    } else{
-        nav.id = "";
-    }
-});
+    window.scrollY > 200 ? nav.id = "fixed-nav" : nav.id = "";
+}
+);

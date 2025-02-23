07aa2825c68b0c7b670b59f553015df29530efca
OrMichal <misaorsah5@gmail.com>
2025-02-23 15:05:08 +0100

    už fungujou automatizovaný e-maily a tím pádem je dokončena část " O nás".

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const nodemailer = require('nodemailer');
+const Mail = require('nodemailer/lib/mailer');
 
 const app = express();
 const port = 3000;
+const app_mail = "daisytestmail01@gmail.com";
 
 app.use(cors());
 app.use(express.json());
@@ -16,35 +18,38 @@ mongoose.connect("mongodb://localhost:27017/dbDaisy")
     .then(() => console.log("connected to mongoDB"))
     .catch((err) => console.log("error occurred during mongo connection",err));
 
-const _mail = nodemailer.createTransport({
+    
+const transporter = nodemailer.createTransport({
+    secure: true,
+    host: "smtp.gmail.com",
+    port: 465,
     service: "gmail",
     auth: {
         user: "daisytestmail01@gmail.com",
-        pass:"Dt123456"
-    }
+        pass:"bgbywgbaoadkyjtq"
+    },
 });
 
-_mail.sendMail({
-    from: "daisytestmail01@gmail.com",
-    to: "misaorsah5@gmail.com",
-    subject: "test",
-    text: "testovací zpráva"
-}, (error, info) => error ? console.log(error) : console.log("email sent: ", info.response));
+function SendMail(to, subject, Message){
+    transporter.sendMail({
+        from: app_mail,
+        to: to,
+        subject: subject,
+        text: Message
+    }, (error, info) => error ? console.log("error during sending mail: ",error) : console.log("Email sent!"));
+}
 
-app.post("/sendEmail", (req, res) => {
+
+app.post("/sendEmail", async (req, res) => {
     console.log("received request");
-    
-    const {Name, Address, PhoneNumber, UserMsg} = req.body.json();
-    _mail.sendMail({
-        from: "daisytestmail01@gmail.com",
-        to: Address,
-        subject: "Received message",
-        text: "Dobrý den, \n přijali jsme vaši zprávu: " + UserMsg
-    }, (error, info) => {
-        if(error){
-            console.log(error);
-            return;
-        }
-        console.log("Email sent: ", info.response);
-    });
+    try {
+        const {Name, Address, PhoneNumber, UserMsg} = req.body;
+        SendMail(Address, 
+            "Přijat dotaz", 
+            "Dobrý den vážený pane / vážená paní "+ Name +", \n přijali jsme vaši zprávu z našeho webu: \n \n \t" + UserMsg + "\n \n odpovíme vám případně na vaše telefonní číslo: " + PhoneNumber);
+        
+    } catch (err) {
+        console.log("mail error:", err);
+        return res.status(500).json({Message: "Internal server error:"})
+    }
 });
